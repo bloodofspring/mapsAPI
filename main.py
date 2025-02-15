6b3132c2a2b5836fead25ea8903f732fcfc33463
SIlD07 <sychilia07@gmail.com>
2025-02-15 21:20:28 +0300

    something normal

--- main.py
+++ main.py
@@ -16,25 +16,21 @@ def setup_screen():
 def request_map_image(ll, spn):
     map_params = {
         'll': ll,
-        'spn': spn
+        'spn': spn,
+        'l': 'map',
+        'apikey': 'f3a0fe3a-b07e-4840-a1da-06f18b2ddf13'
     }
-    server_address = 'https://static-maps.yandex.ru/v1?'
-    api_key = 'f3a0fe3a-b07e-4840-a1da-06f18b2ddf13'
-    ll_spn = 'll=37.530887,55.703118&spn=0.002,0.002'
-    map_request = f"{server_address}{ll_spn}&apikey={api_key}"
-    response = requests.get(map_request)
+    map_request = f'https://static-maps.yandex.ru/v1'
+    response = requests.get(map_request, params=map_params)
+    print(response.request.url)
 
-    map_file = "map.jpg"
-    with open(map_file, "wb") as file:
-        file.write(response.content)
-
-    return map_file
+    return io.BytesIO(response.content)
 
 def main():
     screen = setup_screen()
     parser = argparse.ArgumentParser()
-    parser.add_argument('ll', nargs=2)
-    parser.add_argument('spn', nargs=2)
+    parser.add_argument('--ll', type=str)
+    parser.add_argument('--spn', type=str)
     args = parser.parse_args()
     map_image = request_map_image(ll=args.ll, spn=args.spn)
     screen.blit(pygame.image.load(map_image), (0, 0))
@@ -42,7 +38,6 @@ def main():
     while pygame.event.wait().type != pygame.QUIT:
         pass
     pygame.quit()
-    os.remove(map_image)
 
 
 if __name__ == '__main__':
