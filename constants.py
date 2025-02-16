8ca5ef8bf7e3ac9c5f608aadf605865bb29c31d9
bloodofspring <t.shagin@gmail.com>
2025-02-16 14:28:28 +0300

    feat: completed task 2. ToDo: Delete __pycache__ files from repo

--- constants.py
+++ constants.py
@@ -4,3 +4,6 @@ window_width: Final[int] = 1100
 window_height: Final[int] = 700
 
 fps: Final[int] = 60
+
+top_spn_limit: Final[float] = 0.09
+bottom_spn_limit: Final[float] = 0.005
--- screens/showStaticMaps.py
+++ screens/showStaticMaps.py
@@ -1,11 +1,10 @@
 import argparse
+import io
 
 import pygame
 import requests
-import os
-import io
-from PIL import Image
 
+from constants import bottom_spn_limit, top_spn_limit, window_width, window_height
 from screens.abc import AbstractScreen
 
 
@@ -17,6 +16,8 @@ class ShowStaticMapsScreen(AbstractScreen):
         self.spn = args.spn
         self.last_request_params = {"ll": self.ll, "spn": self.spn}
         self.last_request_image: io.BytesIO | None = None
+        self.show_limit_text: bool = False
+        self.font = pygame.font.Font("static/fonts/pixelFont.TTF", 40)
 
     @property
     def should_update(self):
@@ -37,7 +38,33 @@ class ShowStaticMapsScreen(AbstractScreen):
 
         return io.BytesIO(response.content)
 
+    def change_spn(self, d: float):
+        a, b = map(float, self.spn.split(","))
+
+        if not (bottom_spn_limit <= a + d <= top_spn_limit and bottom_spn_limit <= b + d <= top_spn_limit):
+            self.show_limit_text = True
+            return
+
+        self.show_limit_text = False
+
+        a += d
+        b += d
+        self.spn = ",".join(map(str, (a, b,)))
+
+    def handle_events(self, events):
+        for event in events:
+            if event.type != pygame.KEYUP:
+                continue
+
+            if event.key == pygame.K_UP:  # pygame.K_PAGEUP
+                self.change_spn(-0.005)
+
+            if event.key == pygame.K_DOWN:  # pygame.K_PAGEDOWN
+                self.change_spn(0.005)
+
     def update(self, events, **kwargs):
+        self.handle_events(events)
+
         if self.should_update:
             self.last_request_image = self.request_map_image()
 
@@ -45,4 +72,9 @@ class ShowStaticMapsScreen(AbstractScreen):
         self.last_request_image.seek(0)
 
         pg_image = pygame.image.load(self.last_request_image)
-        self.screen.blit(pg_image, ((1100 - pg_image.get_width()) // 2, (700 - pg_image.get_height()) // 2))
+        self.screen.blit(pg_image,
+                         ((window_width - pg_image.get_width()) // 2, (window_height - pg_image.get_height()) // 2))
+
+        rendered_text = self.font.render("You  reached  scale  limit!", True,
+                                         "white" if self.show_limit_text else "black")
+        self.screen.blit(rendered_text, ((window_width - rendered_text.get_width()) // 2, window_height - 50))
